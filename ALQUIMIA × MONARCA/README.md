0d827f3bc6bd94263b1874e9af62194b08e2167e
ZZTX-zztx <3809026566@qq.com>
2026-08-13 10:24:03 +0800

    Update 2026-08-13 10:24:03

---
+++ ALQUIMIA × MONARCA/README.md
@@ -0,0 +1,3 @@
+# ALQUIMIA × MONARCA
+《ALQUIMIA × MONARCA》
+自制混搭 Montagem Beat，将炼金系 Alquimia 旋律与王权 Monarca 鼓点融合重构，重新编排节奏、音色切片，打造双重质感巴西 Montagem 电子曲。
---
+++ main.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('.config-btn');
+btn.addEventListener('click', () => {
+    alert('跳转到页面配置界面');
+    // window.location.href = "config.html"; // 填写配置页面地址
+})
