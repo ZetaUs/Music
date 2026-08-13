e6a75502b3f05556daae4d4c9585d802ac2a96d2
ZZTX-zztx <3809026566@qq.com>
2026-08-13 12:05:32 +0800

    Update 2026-08-13 12:05:32

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const btn = document.querySelector('.config-btn');
 btn.addEventListener('click', () => {
-    // 同标签页跳转至 ALQUIMIA × MONARCA 页面
-    window.location.href = "alquimia-monarca.html";
-    // 如需新标签打开用下面这行，注释上面即可
-    // window.open("alquimia-monarca.html", "_blank");
-})
+    const targetPath = '/ALQUIMIA × MONARCA';
+    const safeUrl = encodeURI(targetPath);
+    window.location.href = safeUrl;
+});
