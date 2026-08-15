70d53cc344eccc65c076736aa1a8cff60b5d9cfa
ZZTX-zztx <3809026566@qq.com>
2026-08-15 14:18:13 +0800

    Update 2026-08-15 14:18:13

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector('.config-btn');
 btn.addEventListener('click', () => {
-    const targetPath = '/ALQUIMIA × MONARCA';
+    const targetPath = 'ALQUIMIA × MONARCA/index.html';
     const safeUrl = encodeURI(targetPath);
     window.location.href = safeUrl;
 });
