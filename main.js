bc4c959f556b194ad4e2f1e677dab0a57212fb21
unknown <you@example.com>
2026-08-19 19:34:57 +0800

    Update 2026-08-19 19:34:57

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+// 原有按钮
 const btn = document.querySelector('.config-btn');
 btn.addEventListener('click', () => {
     const targetPath = 'ALQUIMIA × MONARCA/index.html';
+    const safeUrl = encodeURI(targetPath);
+    window.location.href = safeUrl;
+});
+
+// 新增 Internal Scars 按钮逻辑
+const btnInternal = document.querySelectorAll('.config-btn')[1];
+btnInternal.addEventListener('click', () => {
+    const targetPath = './Internal Scars/index.html';
     const safeUrl = encodeURI(targetPath);
     window.location.href = safeUrl;
 });
